645dbfc01085e37ae6b34b3a688790bc5b4b0de2
kerroniom <kerroniom@gmail.com>
2014-11-23 23:43:07 +0000

    Updates with solution
    As per assignment 2 details

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The functions follow the same basic format as the example in the
+## assignment 2 instructions, first creates a function to produce a matrix
+## that can cash its inverse, the second either gets the inverse of the matrix
+## from the cash, or calculates it if it isnt there
+
 ## Write a short comment describing this function
+## see above comments first part
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( m = matrix() ) {
+  i <- NULL
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  get <- function() m
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## see above comments second part
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
 }
